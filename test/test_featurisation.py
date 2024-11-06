14d101d994d694a70745a5d041997c79b6aac3b5
Hofstra <martijn.hofstra@eneco.com>
2024-11-06 18:07:45 +0100

    create mock data for feature unit test

--- test/test_featurisation.py
+++ test/test_featurisation.py
@@ -1,6 +1,5 @@
-from unittest.mock import MagicMock
-
 import pytest
+from pyspark.sql import DataFrame, SparkSession
 
 from hotel_reservations.featurisation.featurisation import write_feature_table
 from test.utils import spark_session
@@ -9,10 +8,16 @@ spark = spark_session
 
 
 @pytest.fixture
-def feature_data():
+def feature_data(spark: SparkSession) -> DataFrame:
     """Fixture to create a mock feature data DataFrame."""
-    data = MagicMock()  # Mocked DataFrame, could replace with actual data for more specific tests
-    data.columns = ["feature1", "feature2", "primary_key"]  # Mock columns for testing
+    # Create a real DataFrame with sample data
+    data = spark.createDataFrame(
+        [
+            ("val1", 1.0, "key1"),
+            ("val2", 2.0, "key2"),
+        ],
+        ["feature1", "feature2", "primary_key"],
+    )
     return data
 
 
