3b4b04ccad20ef8f1a3b751f1d7fc31356917e78
Hofstra <martijn.hofstra@eneco.com>
2024-11-10 07:51:12 +0100

    added type of features for table naming

--- src/hotel_reservations/featurisation/featurisation.py
+++ src/hotel_reservations/featurisation/featurisation.py
@@ -3,7 +3,13 @@ from pyspark.sql import DataFrame, SparkSession
 
 
 def write_feature_table(
-    feature_data: DataFrame, catalog: str, schema: str, use_case_name: str, primary_key: str, spark: SparkSession
+    feature_data: DataFrame,
+    catalog: str,
+    schema: str,
+    use_case_name: str,
+    features_type: str,
+    primary_key: str,
+    spark: SparkSession,
 ) -> str:
     """Write feature data to the databricks Feature Store. If the table already exists, the data will be upserted. If not, then a table will be created in the Feature Store.
 
@@ -12,6 +18,7 @@ def write_feature_table(
         catalog (str): Catalog in which to write the feature data
         schema (str): Schema/database in which to write the feature data
         use_case_name (str): Name of the use case
+        features_type (str): Type of features for clear naming of the feature table, e.g. 'predictions_features'
         primary_key (str): Name of the column to use as PK in the Feature table
         spark(SparkSession): The SparkSession used for writing to the FS
 
@@ -21,7 +28,7 @@ def write_feature_table(
     if primary_key not in feature_data.columns:
         raise ValueError(f"Primary key column '{primary_key}' not found in feature_data")
 
-    feature_table_name = f"{catalog}.{schema}.{use_case_name}_features"
+    feature_table_name = f"{catalog}.{schema}.{use_case_name}_{features_type}"
     fe = FeatureEngineeringClient()
 
     if spark.catalog.tableExists(feature_table_name):
